f436ae6288eb8b24d6f062bdf87c24f984807f1e
triadonoss <85204936+triadonoss@users.noreply.github.com>
2023-02-06 22:16:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chat-gpt3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-sxzkum)
---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [prompts, setPrompts] = useState('');
+  const [suggestion, setSuggestion] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const models = async () => {
+    setIsLoading(true);
+    const list = await fetch('https://tan-clumsy-squid.cyclic.app', { // https://nice-erin-basket-clam-tie.cyclic.app
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        promps: prompts,
+      }),
+    });
+
+    return list.json();
+  };
+
+  useEffect(() => {
+    if (suggestion) {
+      setIsLoading(false);
+    }
+  }, [suggestion]);
+  const handleChange = (value) => {
+    setPrompts(value);
+    setSuggestion('');
+  };
+  const handleClick = async () => {
+    const res = await models();
+    setSuggestion(res[0]?.text);
+  };
+
+  return (
+    <div className="App">
+      <h1>
+        <span>GPT-3 Completion App</span> <br />{' '}
+        <span>"text-davinci-003" model from OpenAI</span>
+      </h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="input-completion">
+          Type text suggestion
+        </label>
+        <br />
+        <input
+          id="input-completion"
+          type="text"
+          placeholder="type here and get suggestions"
+          value={prompts}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <br />
+        <div>
+          {!suggestion && !isLoading && <p>...</p>}
+          {isLoading && <p>loading</p>}
+          {suggestion}
+        </div>
+        <button onClick={handleClick}>Get Completion Text</button>
+        <br />
+        <small>Implemented by RedLds</small>
+      </form>
+    </div>
+  );
+}
+
+export default App;
